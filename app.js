a6bc17322ed70209163914bed8e88444304d60c1
Lauramars <lauramars85@gmail.com>
2020-08-05 14:55:48 +0200

    Calculate & Error

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-alert(1);
+// Listen for submit
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('calculating');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  // Turns the amount into decimal
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute montly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    /*here I'm sayng I want 2 decimals*/
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError('Please, check your numbers');
+  }
+  e.preventDefault();
+}
+
+// Show error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create textNode and append to Div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error in card, above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 sec.
+  setTimeout(clearError, 3000);
+}
+
+// clear error
+function clearError() {
+  document.querySelector('.alert').remove();
+}
