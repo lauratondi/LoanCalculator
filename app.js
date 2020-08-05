06439009962dd10b2f9f36a56db39c3661f49be0
Lauramars <lauramars85@gmail.com>
2020-08-05 15:34:39 +0200

    Loader & UE

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // Listen for submit
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log('calculating');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -28,14 +35,25 @@ function calculateResults(e) {
     /*here I'm sayng I want 2 decimals*/
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    //   Show results
+    document.getElementById('results').style.display = 'block';
+
+    //   Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please, check your numbers');
   }
-  e.preventDefault();
 }
 
 // Show error
 function showError(error) {
+  //   Hide results
+  document.getElementById('results').style.display = 'none';
+
+  //   Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
